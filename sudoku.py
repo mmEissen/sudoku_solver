6875c945833cd00a2b0bce509d9c926156b3e41f
Moritz Eissenhauer <m.eissenhauer@reckondigital.com>
2017-10-09 13:55:42 +0100

    change line lengths to 80

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,3 @@
-
 import itertools
 import re
 
@@ -17,16 +16,22 @@ def create_solver_vars(sudoku, solver):
     """Creates and returns IntVars for a given sudoku.
     """
     solver_vars = [
-        solver.IntVar(number, number, str(i)) if number else solver.IntVar(1, PUZZLE_SIZE, str(i))
+        solver.IntVar(number, number, str(i)) if number
+        else solver.IntVar(1, PUZZLE_SIZE, str(i))
         for i, number in enumerate(sudoku)
     ]
     return solver_vars
 
 def create_constraints(solver_vars, solver):
-    """Creates constraints on the `solver_vars` and add the constraints to the solver.  
+    """Creates constraints on the `solver_vars` and add the constraints to the
+    solver.
     """
-    lines = [solver_vars[i:i + PUZZLE_SIZE] for i in range(0, PUZZLE_SIZE ** 2, PUZZLE_SIZE)]
+    lines = [
+        solver_vars[i:i + PUZZLE_SIZE]
+        for i in range(0, PUZZLE_SIZE ** 2, PUZZLE_SIZE)
+    ]
     rows = zip(*lines)
+
     cell_coords = itertools.product(range(0, PUZZLE_SIZE, CELL_SIZE), repeat=2)
     cells = [
         [
@@ -38,6 +43,7 @@ def create_constraints(solver_vars, solver):
         ]
         for cell_x, cell_y in cell_coords
     ]
+
     for section in itertools.chain(lines, rows, cells):
         solver.Add(solver.AllDifferent(section))
 
@@ -78,7 +84,7 @@ def print_sudoku(sudoku):
     print()
 
 def load_sudokus(file_name):
-    """Load sudokus from a file. The file is expected to have one sudoku in every line
+    """Load sudokus from a file. The file ichs expected to have one sudoku in every line
     """
     with open(file_name) as sudoku_file:
         sudokus = [[int(c) for c in line.strip()] for line in sudoku_file]
@@ -86,7 +92,7 @@ def load_sudokus(file_name):
 
 def main():
     sudokus = load_sudokus('sudoku_data.txt')
-    for sudoku in sudokus:
+    for sudoku in sudokus[:50]:
         solved_sudoku = solve(sudoku)
         print_sudoku(solved_sudoku)
 
